fc505183710a7188ac5bab1e4ff22b27020d6e29
Videl90 <lorena.sa.0117@gmail.com>
2020-07-22 14:05:19 -0500

    handlebars working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
+var db = require("../models");
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -25,8 +26,12 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/addactivity.html"));
   });
 
-  app.get("/bucketlist", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/bucketList.html"));
+  app.get("/bucketlist", async(req, res) => {
+    //res.sendFile(path.join(__dirname, "../public/bucketList.html"));
+    db.Post.findAll({}).then(function(data) {
+      console.log('All Posts: ', data)
+      res.render("index", {data});
+    });
   });
 
   // Here we've add our isAuthenticated middleware to this route.
